9b0afd233878c8f13916bd0881aa1c3b8265e3aa
Musa <edjutaweedawit@gmail.com>
2025-08-01 12:58:53 -0700

    src: add package initializer

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+"""Top-level package for the RL chess agent.
+
+This module exposes the public API of the library.  It also ensures that
+relative imports within the package work when run as a module.
+"""
+
+__all__ = [
+    "envs",
+    "train",
+    "eval",
+    "elo",
+    "utils",
+]

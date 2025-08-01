1ca92d740d59c470581ad464c8a7630746a71f11
Musa <edjutaweedawit@gmail.com>
2025-08-01 12:59:47 -0700

    utils: add package initializer

---
+++ src/utils/__init__.py
@@ -0,0 +1,9 @@
+"""Utility functions for the RL chess agent.
+
+This package contains modules for logging and plotting training and evaluation metrics.
+"""
+
+__all__ = [
+    "logger",
+    "plotting",
+]
